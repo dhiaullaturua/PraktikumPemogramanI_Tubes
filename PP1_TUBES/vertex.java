51be34fab08250dad2200980f0a6bbf38f6bc4c1
muhammadandriansyah <147692926+muhammadandriansyah@users.noreply.github.com>
2025-06-09 10:50:43 +0700

    fix

---
+++ PP1_TUBES/vertex.java
@@ -0,0 +1,13 @@
+package SPP;
+
+public class Vertex {
+	
+	 String name;
+	    boolean visited;
+
+	    public Vertex(String name) {
+	        this.name = name;
+	        this.visited = false;
+	    }
+
+}
